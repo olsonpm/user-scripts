96aa96ff844425d031d28c31e7e8839aa163a3cb
phil olson <philip.olson@pm.me>
2020-03-29 22:42:46 -0500

    remove obsolete comments

--- mail.protonmail.com.user.js
+++ mail.protonmail.com.user.js
@@ -8,15 +8,6 @@
 // @updateURL      https://raw.githubusercontent.com/olsonpm/user-scripts/dev/mail.protonmail.com.user.js
 // ==/UserScript==
 
-//
-// README
-//  - This script creates a hook for whenever the location changes.  That way we
-//    can listen to that and remove the unread counter in the title accordingly
-//
-//    Most of the code is from here
-//    https://stackoverflow.com/a/52809105/984407
-//
-
 setInterval(fixTitle, 100);
 
 function fixTitle() {
